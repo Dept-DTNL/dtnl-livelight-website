40244ab1aa19f3200af3da8cabe90b935dc9ed5c
Gianni Hoffstedde <gianni.hoffstedde@deptagency.com>
2022-07-08 16:03:19 +0200

    Hotfix migration added

--- DTNL.LL.DAL/Migrations/20220708140246_veryhightraffic.Designer.cs
+++ DTNL.LL.DAL/Migrations/20220708140246_veryhightraffic.Designer.cs
@@ -4,14 +4,16 @@ using DTNL.LL.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DTNL.LL.DAL.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220708140246_veryhightraffic")]
+    partial class veryhightraffic
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DTNL.LL.DAL/Migrations/20220708140246_veryhightraffic.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DTNL.LL.DAL.Migrations
+{
+    public partial class veryhightraffic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EffectCooldownInMinutes",
+                table: "LifxLights",
+                type: "int",
+                nullable: false,
+                defaultValue: 5);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PulseAmount",
+                table: "LifxLights",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VeryHighTrafficAmount",
+                table: "LifxLights",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VeryHighTrafficCycleTime",
+                table: "LifxLights",
+                type: "float",
+                nullable: false,
+                defaultValue: 1.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VeryHighTrafficFirstColor",
+                table: "LifxLights",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "VeryHighTrafficSecondColor",
+                table: "LifxLights",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EffectCooldownInMinutes",
+                table: "LifxLights");
+
+            migrationBuilder.DropColumn(
+                name: "PulseAmount",
+                table: "LifxLights");
+
+            migrationBuilder.DropColumn(
+                name: "VeryHighTrafficAmount",
+                table: "LifxLights");
+
+            migrationBuilder.DropColumn(
+                name: "VeryHighTrafficCycleTime",
+                table: "LifxLights");
+
+            migrationBuilder.DropColumn(
+                name: "VeryHighTrafficFirstColor",
+                table: "LifxLights");
+
+            migrationBuilder.DropColumn(
+                name: "VeryHighTrafficSecondColor",
+                table: "LifxLights");
+        }
+    }
+}
